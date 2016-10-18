198c9f05f4236564542fe3827d8056e0c826689d
Travis-R-Ristow <TRtheGreat19@gmail.com>
2016-10-17 17:25:21 -0700

    Add files via upload

--- regex_challenge.py
+++ regex_challenge.py
@@ -21,5 +21,47 @@ def get_matching_words(regex):
 
     return results
 
-my_expression = r"a"
+my_expression = r"."
+print 'all'
 print get_matching_words(my_expression)
+print '-'*100
+one = r'ss'
+print 'one'
+print get_matching_words(one)
+print '-'*100
+two = r'b\wb'
+print 'two'
+print get_matching_words(two)
+print '-'*100
+three = r'b\w+b'
+print 'three'
+print get_matching_words(three)
+print '-'*100
+four = r'b\w*b'
+print 'four'
+print get_matching_words(four)
+print '-'*100
+five = r'b\w?b'
+print 'five'
+print get_matching_words(five)
+print '-'*100
+six = r'a\w*e\w*i\w*o\w*u'
+print 'six'
+print get_matching_words(six)
+print '-'*100
+seven = r'[aeiou]{2}$'
+print 'seven'
+print get_matching_words(seven)
+print '-'*100
+eight = r'^[regulaxpsion]*$'
+print 'eight'
+print get_matching_words(eight)
+print '-'*100
+nine = r'^[^regx]*$'
+print 'nine'
+print get_matching_words(nine)
+print '-'*100
+ten = r'l{2}\w*l{2}'
+print 'ten'
+print get_matching_words(ten)
+print '-'*100
